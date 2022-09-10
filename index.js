02ba21d59e324eb50b81cff44338e47278025a26
mhakmi <marwahakmi@gmail.com>
2022-09-10 11:01:55 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+const submitData = (name, email) => {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }),
+    }
+    const res = fetch('http://localhost:3000/users', configurationObject)
+    return res.then(data => data.json())
+        .then((data) => {
+            document.body.innerHTML = data.id
+        })
+        .catch(function (error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+            document.body.innerHTML = 'Unauthorized Access'
+        });
+}
+
+submitData('Steve', "steve@steve.com")
